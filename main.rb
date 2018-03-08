781dbe7b2846b397619ee698bcfe2dfed75c771c
orison09 <32190726+orison09@users.noreply.github.com>
2018-03-09 00:03:46 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+class ShortStringPacker
+  ## Packs a short string into a Fixnum
+  # Arguments:
+  #   str - String object
+  # Returns: a Fixnum object
+	
 def self.pack(str)
   array = []
 	key = {"a"=> 1, "b"=> 2,"c"=>3,"d"=>4,"e"=>5,"f"=>6,"g"=>7,"h"=>8,"i"=>9,"j"=>10,"k"=>11,"l"=>12,"m"=>13,"n"=>14,"o"=>15,"p"=>16,"q"=>17,"r"=>18,"s"=>19,"t"=>20,"u"=>21,"v"=>22,"w"=>23,"x"=>24,"y"=>25,"z"=>26}
@@ -21,9 +27,6 @@ def self.pack(str)
 end
 
 
-
-
-
 def self.unpack(int)
     str = int.to_s
     puts str
@@ -34,7 +37,7 @@ end
 
 
 
-test =  self.pack("muzi")
+test =  self.pack("orison")
 ##puts test 
 
 self.unpack(test)
